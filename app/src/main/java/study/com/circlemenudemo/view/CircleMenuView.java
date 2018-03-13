1acda20d5e37dd067a0bd56748cb9e36e3e60811
Duckdan <1475535138@qq.com>
2018-03-13 17:46:33 +0800

    提交圆形菜单的旋转

--- app/src/main/java/study/com/circlemenudemo/view/CircleMenuView.java
+++ app/src/main/java/study/com/circlemenudemo/view/CircleMenuView.java
@@ -3,10 +3,12 @@ package study.com.circlemenudemo.view;
 
 import android.content.Context;
 import android.util.AttributeSet;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import study.com.circlemenudemo.R;
 
@@ -19,6 +21,7 @@ public class CircleMenuView extends ViewGroup {
     private int height;
     private int min;
     private float radius;
+    private Context context;
 
     public CircleMenuView(Context context) {
         this(context, null);
@@ -30,6 +33,8 @@ public class CircleMenuView extends ViewGroup {
 
     public CircleMenuView(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
+        this.context = context;
+
         init();
     }
 
@@ -61,18 +66,17 @@ public class CircleMenuView extends ViewGroup {
         radius = widthSize * 1.0f / 2;
         widthMeasureSpec = MeasureSpec.makeMeasureSpec(widthSize, widthMode);
         setMeasuredDimension(widthMeasureSpec, widthMeasureSpec);
-        radius = MeasureSpec.getSize(widthMeasureSpec);
         for (int i = 0; i < getChildCount(); i++) {
             View child = getChildAt(i);
             //给child制定测量规则
             int size = (int) (radius / 3);
 
             TextView tv = (TextView) child.findViewById(R.id.tv);
-            tv.setTextSize(size * 0.05f);
+            tv.setTextSize(size * 0.08f);
             ImageView iv = (ImageView) child.findViewById(R.id.iv);
             LayoutParams ivLayoutParams = iv.getLayoutParams();
-            ivLayoutParams.width = (int) (size * 0.5f);
-            ivLayoutParams.height = (int) (size * 0.5f);
+            ivLayoutParams.width = (int) (size * 0.7f);
+            ivLayoutParams.height = (int) (size * 0.7f);
             iv.setLayoutParams(ivLayoutParams);
 
             int measureSpec = MeasureSpec.makeMeasureSpec(size, MeasureSpec.EXACTLY);
@@ -92,7 +96,6 @@ public class CircleMenuView extends ViewGroup {
         width = w;
         height = h;
         min = Math.min(w, h);
-        radius = min * 1.0f / 2;
     }
 
     private int left;
@@ -118,21 +121,78 @@ public class CircleMenuView extends ViewGroup {
         }
     }
 
+
     /**
      * 设置数据
      *
      * @param texts
      * @param resIds
      */
-    public void setData(int[] resIds, String[] texts) {
+    public void setData(int[] resIds, final String[] texts) {
         for (int i = 0; i < resIds.length; i++) {
             View view = View.inflate(getContext(), R.layout.circle_item, null);
+            final String text = texts[i];
+            view.setOnClickListener(new OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Toast.makeText(context, text, Toast.LENGTH_SHORT).show();
+                }
+            });
             ImageView iv = (ImageView) view.findViewById(R.id.iv);
             TextView tv = (TextView) view.findViewById(R.id.tv);
             iv.setImageResource(resIds[i]);
             tv.setText(texts[i]);
-
             addView(view);
         }
     }
+
+
+    float downAngle = 0;
+    float moveAngle = 0;
+    float changeAngle;
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        float x = event.getX();
+        float y = event.getY();
+
+        changeAngle = 0;
+        switch (event.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                downAngle = xyToAngle(x - radius, y - radius);
+                break;
+            case MotionEvent.ACTION_MOVE:
+                moveAngle = xyToAngle(x - radius, y - radius);
+                changeAngle = (moveAngle - downAngle) % 360;
+
+                startAngle += changeAngle;
+                requestLayout();
+                downAngle = moveAngle;
+                break;
+            case MotionEvent.ACTION_UP:
+                break;
+        }
+        // starAngle = 0;
+        return true;
+    }
+
+    //转化角度
+    public float xyToAngle(float x, float y) {
+        float transAngle = (float) Math.toDegrees(Math.atan2(y, x));  //计算得出的值的范围为[-π,π];
+        return transAngle >= 0 ? transAngle : transAngle + 360;
+    }
+
+//    private float xyToAngle(float downX, float downY) {
+//        float originalAngle = 0f;
+//        if (downX > 0 && downY < 0) {  //第四象限
+//            originalAngle += 360;
+//        } else if (downX < 0 && downY < 0) {  //第三象限
+//            originalAngle += 180;
+//        } else if (downX < 0 && downY > 0) {  //第二象限
+//            originalAngle += 180;
+//        }
+//        originalAngle = (float) (originalAngle + Math.toDegrees(Math.atan(downY / downX))) % 360;
+//        return originalAngle;
+//    }
+
 }
